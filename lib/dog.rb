198ed85b16b6624869c9d6f0341c1669a026d7a9
geogrey12muiruri <greggambugua@gmail.com>
2022-10-23 10:22:47 +0300

    commit tree a2dc71861afb0de569745a8f8c6466b019315ca6

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+require "pry"
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+#methods can only do what we teach them to do via the code we write and the methods we define
+#for example our dof 'fido' knows how to bark but cannot sit
+
+#in the same manner, instance methods can be defined as methods that belong to a partiular instance of a particular class methods cannot be called without an instance
